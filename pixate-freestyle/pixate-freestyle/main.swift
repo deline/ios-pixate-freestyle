544b3b031adedfe40391c1b339f0bc4391bf0d50
deline <delineneo@gmail.com>
2015-09-08 12:58:00 +1000

    Add basic button styles for classy and pixate-freestyle projects

---
+++ pixate-freestyle/pixate-freestyle/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  pixate-freestyle
+//
+//  Created by Deline Neo on 8/09/2015.
+//  Copyright © 2015 Deline Neo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+// Your initialization code here
+
+PixateFreestyle.initializePixateFreestyle()
+UIApplicationMain(Process.argc, Process.unsafeArgv, nil, NSStringFromClass(AppDelegate))
---
+++ pixate-freestyle/pixate-freestyle/pixate-freestyle-BridgingHeader.h
@@ -0,0 +1,9 @@
+//
+//  pixate-freestyle-BridgingHeader.h
+//  pixate-freestyle
+//
+//  Created by Deline Neo on 8/09/2015.
+//  Copyright © 2015 Deline Neo. All rights reserved.
+//
+
+#import <PixateFreestyle/PixateFreestyle.h>
